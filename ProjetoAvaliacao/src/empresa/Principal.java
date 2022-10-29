b29d0105dc5f712fb080cc6a66a73dba801df597
2018@Arthur <shrek18.5@gmail.com>
2022-10-29 20:31:38 -0300

    Commit Inicial Eclipse para GIT

---
+++ ProjetoAvaliacao/src/empresa/Principal.java
@@ -0,0 +1,22 @@
+package empresa;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Avaliacao luigi = new Avaliacao();
+		Avaliacao mario = new Avaliacao(7,4,10);
+		luigi.n1 = 8;
+		luigi.n2 = 3;
+		luigi.n3 = 7.5;
+		
+		
+		System.out.println("Media Aritimetica do Luigi: " + luigi.mediaAritmetica());
+		System.out.println("Media Ponderada do Luigi: " + luigi.mediaPonderada());
+		System.out.println();
+		System.out.println("Media Aritimetica do Mario: " + mario.mediaAritmetica());
+		System.out.println("Media Ponderada do Mario: " + mario.mediaPonderada());
+
+	}
+
+}
